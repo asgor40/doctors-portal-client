d7551ed822f3c8e78e59192fe8e2c63e6ae67613
Asgor Ali <i.joni40@gmail.com>
2022-05-10 20:08:58 +0600

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/Home/About';
 import Home from './Pages/Home/Home';
+import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="px-12">
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
 const Navbar = () => {
+    const MenuItem = <>
+        <li><a>Home</a></li>
+        <li><a>About</a></li>
+        <li><a>Appointment</a></li>
+        <li><a>Reviews</a></li>
+        <li><a>Contact Us</a></li>
+        <li><a>Login</a></li>
+    </>
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <section>
+            <div class="navbar bg-base-100">
+                <div class="navbar-start">
+                    <div class="dropdown">
+                    <label tabindex="0" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {MenuItem}
+                    </ul>
+                    </div>
+                    <a class="btn btn-ghost normal-case text-xl">Doctors Portal</a>
+                </div>
+                <div class="navbar-end hidden lg:flex">
+                    <ul class="menu menu-horizontal p-0">
+                        {MenuItem}
+                    </ul>
+                </div>
+                </div>
+        </section>
     );
 };
 
